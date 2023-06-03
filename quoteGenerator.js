8f73d96bac252553c338b4aeef2861f3ceec2bec
Cristian Lotero Aquino <miultimomail@hotmail.com>
2023-06-03 01:22:10 -0300

    spanish quotes

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,25 +1,200 @@
-let quote0 = [{"El único modo de hacer un gran trabajo es amar lo que haces"},"Steve Jobs"]
-let quote1 = [{""},]
-let quote2 = [{""},]
-let quote3 = [{""},]
-let quote4 = [{""},]
-let quote5 = [{""},]
-let quote6 = [{""},]
-let quote7 = [{""},]
-let quote8 = [{""},]
-let quote9 = [{""},]
-let quote10 = [{""},]
-let quote11 = [{""},]
-let quote12 = [{""},]
-let quote13 = [{""},]
-let quote14 = [{""},]
-let quote15 = [{""},]
-let quote16 = [{""},]
-let quote17 = [{""},]
-let quote18 = [{""},]
-let quote19 = [{""},]
-let quote20 = [{""},]
-let quote21 = [{""},]
-let quote22 = [{""},]
-let quote23 = [{""},]
-let quote24 = [{""},]
+const quoteSpanishProfesional = [{id: 0, quote:"El único modo de hacer un gran trabajo es amar lo que haces", author: "Steve Jobs"},
+    {id: 1,quote:"Nunca pienso en las consecuencias de fallar un gran tiro… cuando se piensa en las consecuencias se está pensando en un resultado negativo ", author: "Michael Jordan"},
+    {id: 2,quote:"El dinero no es la clave del éxito; la libertad para poder crear lo es ", author: "Nelson Mandela"},
+    {id: 3,quote:"Cuanto más duramente trabajo, más suerte tengo", author: "Gary Player"},
+    {id: 4,quote:"La inteligencia consiste no sólo en el conocimiento, sino también en la destreza de aplicar los conocimientos en la práctica", author: "Aristóteles"},
+    {id: 5,quote:"El trabajo duro hace que desaparezcan las arrugas de la mente y el espíritu", author: "Helena Rubinstein"},
+    {id: 6,quote:"Cuando algo es lo suficientemente importante, lo haces incluso si las probabilidades de que salga bien no te acompañan ", author: "Elon Musk"},
+    {id: 7,quote:"Escoge un trabajo que te guste, y nunca tendrás que trabajar ni un solo día de tu vida", author: "Confucio"},
+    {id: 8,quote:"Un sueño no se hace realidad por arte de magia, necesita sudor, determinación y trabajo duro ", author: "Colin Powell"},
+    {id: 9,quote:"Cuéntamelo y me olvidaré. enséñamelo y lo recordaré. involúcrame y lo aprenderé ", author: "Benjamin Franklin "},
+    {id: 10,quote:"La lógica te llevará de la a a la z. la imaginación te llevará a cualquier lugar", author: "Albert Einstein"},
+    {id: 11,quote:"A veces la adversidad es lo que necesitas encarar para ser exitoso", author: "Zig Ziglar "},
+    {id: 12,quote:"Para tener éxito tu deseo de alcanzarlo debe ser mayor que tu miedo al fracaso ", author: "Bill Cosby"},
+    {id: 13,quote:"Ejecuta tus conocimientos con la maestría del que sigue aprendiendo", author: "Jonathan García-Allen"}
+];
+
+const quoteSpanishShorts = [
+    {id:0 ,quote: "Cuando pierdas, no pierdas la lección", author: "Dalai Lama "},
+    {id:1 ,quote: "No busques los errores, busca un remedio", author: "Henry Ford"},
+    {id:2 ,quote: "La vida es una aventura, atrévete ", author: "Teresa de Calcuta"},
+    {id:3 ,quote: "Tu actitud, no tu aptitud, determinará tu altitud ", author: "Zig Ziglar"},
+    {id:4 ,quote: "Tienes que hacer las cosas que crees que no puedes hacer ", author: "Eleanor Roosevelt"},
+    {id:5 ,quote: "Si te caíste ayer, levántate hoy ", author: "H. G. Wells"},
+    {id:6 ,quote: "Siempre parece imposible... hasta que se hace ", author: "Nelson Mandela"},
+    {id:7 ,quote: "Si no pierdes, no puedes disfrutar de las victorias ", author: "Rafael Nadal"},
+    {id:8 ,quote: "No dejes que el miedo se interponga en tu camino", author: "Babe Ruth"},
+    {id:9 ,quote: "Haz de cada día tu obra maestra", author: "John Wooden "},
+    {id:10 ,quote: "No cuentes los días, haz que los días cuenten", author: "Muhammad Ali"},
+    {id:11 ,quote: "El mejor momento del día es ahora ", author: "Pierre Bonnard"},
+    {id:12 ,quote: "Si la oportunidad no llama, construye una puerta ", author: "Milton Berle"},
+    {id:13 ,quote: "Deja que cada hombre ejerza el arte que domina ", author: "Aristófanes "},
+    {id:14 ,quote: "El valor de una idea radica en su uso", author: "Thomas Edison"},
+];
+
+const quoteSpanishPersonalGrowth = [
+    {id:0 ,quote: "Piensa, sueña, cree y atrévete", author: "Walt Disney"},
+    {id:1 ,quote: "Los obstáculos son esas cosas atemorizantes que ves cuando apartas los ojos de tu meta ", author: "Henry Ford"},
+    {id:2 ,quote: "Asegúrate de que colocas tus pies en el lugar correcto, y luego mantente firme", author: "Abraham Lincoln"},
+    {id:3 ,quote: "Las ideas pueden cambiar la vida. A veces a lo único que tienes que abrir la puerta es a una buena idea ", author: "Jim Rohn"},
+    {id:4 ,quote: "La mejor manera para empezar es callándote y empezar a hacer ", author: "Walt Disney"},
+    {id:5 ,quote: "Nadie se desembaraza de un hábito o de un vicio tirándolo de una vez por la ventana; hay que sacarlo por la escalera, peldaño a peldaño", author: "Mark Twain"},
+    {id:6 ,quote: "Si te caes siete veces, levántate ocho ", author: "Proverbio chino"},
+    {id:7 ,quote: "La perfección no es alcanzable, pero si perseguimos la perfección podemos conseguir la excelencia ", author: "Vince Lombardi"},
+    {id:8 ,quote: "Tienes que esperar cosas de ti mismo antes de poder hacerlas ", author: "Michael Jordan"},
+    {id:9 ,quote: "Transforma tus heridas en sabiduría ", author: "Oprah Winfrey"},
+    {id:10 ,quote: "La edad no es barrera. Es una limitación que pones en tu mente", author: "Jackie Joyner-Kersee"},
+    {id:11 ,quote: "Solo aquellos que se arriesgan a ir demasiado lejos pueden descubrir lo lejos que pueden llegar", author: "T.S. Eliot"},
+    {id:12 ,quote: "Del sufrimiento emergieron las almas más fuertes, los caracteres sólidos tienen cicatrices", author: "Khalil Gibran"},
+    {id:13 ,quote: "El universo no conspira contra ti, pero tampoco se desvía para alinear tu camino", author: "Tim Ferriss"},
+    {id:14 ,quote: "Con autodisciplina casi todo es posible", author: "Theodore Roosevelt"}
+    {id:15 ,quote: "", author: ""},
+    {id:16 ,quote: "", author: ""}
+];
+
+const quoteSpanishCelebrated = [
+    {id:0 ,quote: "La innovación distingue al líder del seguidor ", author: "Steve Jobs "},
+    {id:1 ,quote: "Si puedes soñarlo, puedes hacerlo", author: "Walt Disney"},
+    {id:2 ,quote: "El secreto para salir adelante es comenzar ", author: "Mark Twain"},
+    {id:3 ,quote: "Los obstáculos son esas cosas atemorizantes que ves cuando apartas los ojos de tu meta ", author: "Henry Ford"},
+    {id:4 ,quote: "La vida es como montar en bicicleta. para mantener el equilibrio tienes que avanzar", author: "Albert Einstein"},
+    {id:5 ,quote: "El éxito no es definitivo, el fracaso no es fatídico. Lo que cuenta es el valor para continuar ", author: "Winston Churchill"},
+    {id:6 ,quote: "Sé valiente. Toma riesgos. Nada puede sustituir la experiencia", author: "Paulo Coelho"},
+    {id:7 ,quote: "El futuro pertenece a aquellos que creen en la belleza de sus sueños", author: "Eleanor Roosevelt"},
+    {id:8 ,quote: "El conocimiento es poder ", author: "Francis Bacon"},
+    {id:9 ,quote: "He aprendido que el valor no es la ausencia de miedo, sino el triunfo sobre él. El hombre valiente no es el que no siente miedo, sino el que lo domina ", author: "Nelson Mandela"},
+    {id:10 ,quote: "Solo puede ser feliz siempre el que sabe ser feliz con todo", author: "Confucio "},
+    {id:11 ,quote: "Es la capacidad de resistir a los errores lo que a menudo conduce a un mayor éxito ", author: "J. K. Rwolling"},
+    {id:12 ,quote: "Aferrarse al rencor es como beber veneno y esperar que otra persona muera ", author: " Buda"},
+    {id:13 ,quote: "No pares cuando estés cansado. Para cuando hayas terminado", author: "Marilyn Monroe"},
+    {id:14 ,quote: "El 80% del éxito se basa simplemente en insistir ", author: "Woody Allen"}
+];
+
+const quoteSpanishOfLife = [
+    {id:0 ,quote: "Cree que puedes y casi lo habrás logrado", author: "Theodore Roosevelt"},
+    {id:1 ,quote: "No importa lo que te diga la gente, las palabras y las ideas pueden cambiar el mundo", author: "Robin Williams"},
+    {id:2 ,quote: "Si tienes un sueño y crees en él, corres el riesgo de que se convierta en realidad ", author: "Walt Disney"},
+    {id:3 ,quote: "Nunca se es demasiado viejo para fijar otra meta o para soñar un nuevo sueño ", author: "C. S. Lewis"},
+    {id:4 ,quote: "Lo que no te mata, te hace más fuerte ", author: "Friedrich Nietzsche"},
+    {id:5 ,quote: "El hombre que mueve montañas comienza cargando pequeñas piedras ", author: "Confucio"},
+    {id:6 ,quote: "Buenos amigos, buenos libros y una conciencia tranquila: esa es la vida ideal ", author: "Mark Twain"},
+    {id:7 ,quote: "Un optimista ve una oportunidad en toda calamidad, un pesimista ve una calamidad en toda oportunidad ", author: "Winston Churchill"},
+    {id:8 ,quote: "Solo aquellos que se arriesgan a caer pueden conseguir grandes cosas ", author: "Robert F. Kennedy"},
+    {id:9 ,quote: "El éxito es la suma de pequeños esfuerzos, que se repiten día tras día ", author: "Robert Collier"},
+    {id:10 ,quote: "Sal de tu zona de confort. Solo puedes crecer si estás dispuesto a sentirte raro e incómodo cuando intentas algo nuevo", author: "Brian Tracy"},
+    {id:11 ,quote: "¿Por qué contentarnos con vivir a rastras cuando sentimos el anhelo de volar? ", author: "Hellen Keller"},
+    {id:12 ,quote: "Entrega siempre más de lo que esperan de ti ", author: "Larry Page"},
+    {id:13 ,quote: "Aprovecha las oportunidades que aparezcan, donde quiera que estén ", author: "Lakshmi Mittal "},
+    {id:14 ,quote: "Nunca es demasiado tarde para ser lo que podrías haber sido", author: "George Eliot"}
+];
+
+const quoteSpanishPositive = [
+    {id:0 ,quote: "Puedo aceptar el fracaso pero no puedo aceptar no intentarlo ", author: "Michael Jordan"},
+    {id:1 ,quote: "La mejor forma de predecir el futuro es creándolo", author: "Abraham Lincoln "},
+    {id:2 ,quote: "Un día despertarás y descubrirás que no tienes más tiempo para hacer lo que soñabas. El momento es ahora. Actúa ", author: "Paulo Coelho"},
+    {id:3 ,quote: "Hoy eres un lector y mañana serás un líder ", author: "Margaret Fuller"},
+    {id:4 ,quote: "Los dos guerreros más poderosos son la paciencia y el tiempo ", author: "León Tolstói"},
+    {id:5 ,quote: "No puedes derrotar a la persona que nunca se rinde", author: "Babe Ruth"},
+    {id:6 ,quote: "El secreto de tu éxito está determinado por tu agenda diaria ", author: "John C. Maxwell"},
+    {id:7 ,quote: "Recordar que vas a morir, es la mejor manera que conozco para no pensar que tienes algo que perder. Ya estás desnudo. No hay ninguna razón para no seguir a tu corazón", author: "Steve Jobs"},
+    {id:8 ,quote: "Se lo suficientemente valiente para vivir de forma creativa. El lugar creativo donde nadie ha estado ", author: "Alan Alda"},
+    {id:9 ,quote: "Cuando dejo ir lo que soy, me convierto en lo que debería ser ", author: "Lao Tzu"},
+    {id:10 ,quote: "Apunta a la luna. Si fallas, podrías dar a una estrella", author: "William Clement Stone "},
+    {id:11 ,quote: "Existe al menos un rincón del universo que con toda seguridad puedes mejorar, y eres tú mismo ", author: "Aldous Huxley"},
+    {id:12 ,quote: "El verdadero buscador crece y aprende, y descubre que siempre es el principal responsable de lo que sucede", author: "Jorge Bucay"},
+    {id:13 ,quote: "Escribir es un oficio que se aprende escribiendo", author: "Simone de Beauvoir"},
+    {id:14 ,quote: "La forma más rápida de cambiar es convivir con personas que ya son como quieres ser", author: "Reid Hoffman"}
+];
+
+const quoteSpanishBeHappy = [
+    {id:0 ,quote: "La felicidad no es algo que pospones para el futuro; es algo que diseñas para el presente", author: "Jim Rohn"},
+    {id:1 ,quote: "La alegría es contagiosa y siempre consigue descubrir una solución donde la lógica sólo encontró una explicación para el error", author: "Paulo Coelho"},
+    {id:2 ,quote: "Vive la vida que amas. Ama la vida que vives ", author: "Bob Marley"},
+    {id:3 ,quote: "El éxito no es la clave de la felicidad. La felicidad es la clave del éxito ", author: "Albert Schweitzer"},
+    {id:4 ,quote: "Unas palabras amables, un libro cálido y una sonrisa honesta pueden hacer milagros ", author: "William Hazlitt"},
+    {id:5 ,quote: "La verdadera felicidad consiste en hacer el bien ", author: "Aristóteles"},
+    {id:6 ,quote: "El otoño se convierte en una nueva primavera cuando cada hoja es una flor ", author: "Albert Camus"},
+    {id:7 ,quote: "El éxito es encontrar satisfacción en dar un poco más de lo que se recibe ", author: "Christopher Reeve"},
+    {id:8 ,quote: "La alegría reside en la alegría del logro y en la emoción del esfuerzo creativo", author: "Franklin Roosevelt"},
+    {id:9 ,quote: "La verdadera motivación procede de trabajar en cosas que nos importan", author: "Sheryl Sandberg"},
+    {id:10 ,quote: "La felicidad no ocurre por casualidad, sino por elección", author: "Jim Rohn"},
+    {id:11 ,quote: "No tienes poder sobre los acontecimientos exteriores , pero sí sobre tu mente. Aprende esto, y encontrarás tu fuerza ", author: "Marco Aurelio"},
+    {id:12 ,quote: "La buena vida es una forma de pensar ", author: "Emilio Valcárcel"},
+    {id:13 ,quote: "Puedes ser feliz allí donde estés ", author: "Joel Osteen"},
+    {id:14 ,quote: "El término «felicidad» perdería todo su significado si no fuese compensado por la tristeza ", author: "Carl Jung"},
+    {id:15 ,quote: "", author: ""},
+    {id:16 ,quote: "", author: ""}
+];
+
+const quoteSpanishInspiring = [
+    {id:1 ,quote: "Alguien se sienta en la sombra porque alguien plantó un árbol hace mucho tiempo", author: "Warren Buffett"},
+    {id:2 ,quote: "Avanza con confianza en la dirección de tus sueños ", author: "Thoreau"},
+    {id:3 ,quote: "La diligencia es la madre de la buena suerte ", author: "Benjamin Franklin"},
+    {id:4 ,quote: "Sólo se vive una vez. Pero si lo haces bien, una vez basta ", author: "Mae West"},
+    {id:5 ,quote: "Da el primer paso con fe. No tienes que ver todas las escaleras, sólo da el  primer paso ", author: "Martin Luther King"},
+    {id:6 ,quote: "Trabajar duro por algo que no te importa se llama estrés. Trabajar duro por algo que te importa de verdad se llama pasión", author: "Simon Sinek"},
+    {id:7 ,quote: "El éxito depende del esfuerzo", author: "Sófocles"},
+    {id:8 ,quote: "Intenta y falla, pero nunca falles en intentarlo ", author: "Jared Leto"},
+    {id:9 ,quote: "No quiero ganarme la vida, quiero vivir", author: "Oscar Wilde"},
+    {id:10 ,quote: "La vida no es un problema a ser resuelto, sino una realidad a experimentar", author: "Søren Aabye Kierkegaard"},
+    {id:11 ,quote: "Háblate a ti mismo como harías con alguien a quien amas", author: "Brene Brown"},
+    {id:12 ,quote: "Por más difícil que parezca la vida, siempre hay algo que puedes hacer y en lo que puedes tener éxito ", author: "Stephen Hawking"},
+    {id:13 ,quote: "El fracaso se convierte en éxito si aprendes de él ", author: "Malcolm Forbes"},
+    {id:14 ,quote: "La mala noticia es que el tiempo vuela. La buena noticia es que tú eres el piloto", author: "Michael Altshuler"},
+    {id:15 ,quote: "La única manera de encontrar los límites de lo posible es ir más allá de lo imposible", author: "Arthur C. Clarke"},
+];
+
+const quoteSpanishNice = [
+    {id:1 ,quote: "Casi todas las cosas buenas que suceden en el mundo, nacen de una actitud de aprecio por los demás ", author: "Dalai Lama"},
+    {id:2 ,quote: "Donde reina el amor, siempre sobran las leyes ", author: "Platón"},
+    {id:3 ,quote: "Amarse a uno mismo es el comienzo de una larga vida romántica ", author: "Oscar Wilde"},
+    {id:4 ,quote: "Nada de lo que vistes es más importante que tu sonrisa", author: "Connie Stevens"},
+    {id:5 ,quote: "En cada forma de vida existe el deseo del amor ", author: "D. H. Lawrence"},
+    {id:6 ,quote: "Todos nos necesitamos los unos a los otros ", author: "Leo Buscaglia"},
+    {id:7 ,quote: "Antes moriría de pasión que de aburrimiento", author: "Vincent van Gogh"},
+    {id:8 ,quote: "Solo hay una felicidad en la vida, amar y ser amado ", author: "George Sand"},
+    {id:9 ,quote: "El amor propio es la fuente de todos los tipos de amor ", author: "Pierre Corneille"},
+    {id:10 ,quote: "Cambias tu vida al cambiar tu corazón ", author: "Max Lucado"},
+    {id:11 ,quote: "La sencillez es una forma de humildad y la sencillez es una señal de la verdadera grandeza ", author: "Vince Lombardi"},
+    {id:12 ,quote: "El alma que hablar puede con los ojos, también puede besar con la mirada ", author: "Gustavo Adolfo Bécquer "},
+    {id:13 ,quote: "El que ama, se hace humilde. Aquellos que aman, por decirlo de alguna manera, renuncian a una parte de su narcisismo ", author: "Sigmund Freud"},
+    {id:14 ,quote: "Sólo porque alguien no te ame como tú quieres, no significa que no te ame con todo su ser", author: "Gabriel García Márquez"},
+    {id:15 ,quote: "El amor no mira con los ojos, sino con el alma", author: "William Shakespeare"}
+];
+
+const quoteSpanishPositiveThinking = [
+    {id:1 ,quote: "La mejor forma de olvidar las malas cosas de la vida es aprender a recordar las cosas buenas ", author: "Mark Amend"},
+    {id:2 ,quote: "Árbol de la esperanza, mantente firme ", author: "Frida Kahlo"},
+    {id:3 ,quote: "El éxito es la capacidad de ir de fracaso en fracaso sin perder el entusiasmo", author: "Winston Churchill"},
+    {id:4 ,quote: "Tus acciones positivas combinadas con los pensamientos positivos generan éxitos ", author: "Shiv Khera"},
+    {id:5 ,quote: "Qué maravilloso es que nadie tenga que esperar ni un segundo para empezar a mejorar el mundo", author: "Ana Frank"},
+    {id:6 ,quote: "Un solo rayo de sol es suficiente para apartar muchas sombras ", author: "San Francisco de Asís"},
+    {id:7 ,quote: "Cuando piensas en positivo, cosas buenas ocurren", author: "Matt Kemp"},
+    {id:8 ,quote: "No podemos ayudar a todos, pero todo el mundo puede ayudar a alguien", author: "Ronald Reagan "},
+    {id:9 ,quote: "Sé el cambio que quieres ver en el mundo", author: "Mahatma Gandhi "},
+    {id:10 ,quote: "Cualquier cosa que la mente del hombre puede concebir y creer, puede ser conseguida", author: "Napoleon Hill"},
+    {id:11 ,quote: "Si supiera que el mundo se acaba mañana, yo, hoy todavía, plantaría un árbol ", author: "Martin Luther King"},
+    {id:12 ,quote: "Es impresionante. La vida cambia muy rápido, de un modo positivo, si la dejas", author: "Lindsey Vonn"},
+    {id:13 ,quote: "La palabra «no» solo significa que empieces otra vez en un nivel superior ", author: "Peter Diamandis"},
+    {id:14 ,quote: "La grandeza nace de pequeños comienzos ", author: "Sir Francis Drake"},
+    {id:15 ,quote: "Pon tu corazón, mente y alma incluso en los actos más pequeños. Ese es el secreto del éxito ", author: "Swami Sivananda"},
+];
+
+// const quoteSpanish = [
+//     {id:1 ,quote: "", author: ""},
+//     {id:2 ,quote: "", author: ""},
+//     {id:3 ,quote: "", author: ""},
+//     {id:4 ,quote: "", author: ""},
+//     {id:5 ,quote: "", author: ""},
+//     {id:6 ,quote: "", author: ""},
+//     {id:7 ,quote: "", author: ""},
+//     {id:8 ,quote: "", author: ""},
+//     {id:9 ,quote: "", author: ""},
+//     {id:10 ,quote: "", author: ""},
+//     {id:11 ,quote: "", author: ""},
+//     {id:12 ,quote: "", author: ""},
+//     {id:13 ,quote: "", author: ""},
+//     {id:14 ,quote: "", author: ""},
+//     {id:15 ,quote: "", author: ""},
+//     {id:16 ,quote: "", author: ""}
+// ];
